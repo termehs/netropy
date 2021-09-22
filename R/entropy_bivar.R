e94fb93c9f9f627648fc94029cebc2d759e99d4a
termeh <termeh.shafie@manchester.ac.uk>
2021-09-22 11:20:49 +0100

    entropy_bivar.R updated

--- R/entropy_bivar.R
+++ R/entropy_bivar.R
@@ -1,5 +1,5 @@
 #' @title Bivariate Entropies
-#' @description Calculates the bivariate entropies
+#' @description Calculates the bivariate entropies between all pairs of variables in a multivariate data set.
 #' @param dat Dataframe with rows as observations and columns as variables. Variables must all be observed or transformed categorical with finite range spaces.
 #' @return Upper triangular matrix giving bivariate entropies between pairs of variables which are given as rows and columns of the matrix.
 #' The univariate entropies are given in the diagonal.
