6bc7a8ecf2bce419f0b9c19486a84ac647427e19
termeh <termeh.shafie@manchester.ac.uk>
2021-11-22 08:58:27 +0000

    redundancy.R updated

--- R/redundancy.R
+++ R/redundancy.R
@@ -7,7 +7,7 @@
 #' @author Termeh Shafie
 #' @references Frank, O., & Shafie, T. (2016). Multivariate entropy analysis of network data. *Bulletin of Sociological Methodology/Bulletin de Méthodologie Sociologique*, 129(1), 45-63.
 #' @examples
-#' # to be added
+#' # to be added.
 #' @export
 #'
 redundancy <- function(dat, dec = 3) {
