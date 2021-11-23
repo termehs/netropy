222cce44ab25517b7e4a51eb3c9372c2c7e240fd
termeh <termeh.shafie@manchester.ac.uk>
2021-11-23 08:36:32 +0000

    assoc_graph.R updated

--- R/assoc_graph.R
+++ R/assoc_graph.R
@@ -5,6 +5,7 @@
 #' @return A ggraph object.
 #' @details  To be completed
 #' @author Termeh Shafie
+#' @seealso \code{\link{joint_entropy}}
 #' @references Frank, O., & Shafie, T. (2016). Multivariate entropy analysis of network data. *Bulletin of Sociological Methodology/Bulletin de Méthodologie Sociologique*, 129(1), 45-63.
 #' @examples
 #' # to be added
