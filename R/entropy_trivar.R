7b32289a46f2f23556965156f4b1f18006ad1aee
termeh <termeh.shafie@manchester.ac.uk>
2022-01-10 11:20:45 +0000

    entropy_trivar.R updated

--- R/entropy_trivar.R
+++ R/entropy_trivar.R
@@ -1,4 +1,4 @@
-#' @title Trivariate Entropies
+#' @title Trivariate Entropy
 #' @description Computes the trivariate entropies of all triples of variables in a multivariate data set.
 #' @param dat Dataframe with rows as observations and columns as variables. Variables must all be observed or transformed categorical with finite range spaces.
 #' @return Dataframe containing all possible triples of variables and their entropies
