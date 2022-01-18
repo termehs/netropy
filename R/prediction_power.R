22799c74241eb2157e85586a2e670aee44351e53
termeh <termeh.shafie@manchester.ac.uk>
2022-01-18 21:33:16 +0000

    prediction_power.R updated

--- R/prediction_power.R
+++ R/prediction_power.R
@@ -3,18 +3,20 @@
 #' Computes prediction power when pairs of variables in a given dataframe are used 
 #' to predict a third variable from the same dataframe. The prediction strength is measured by
 #' expected conditional entropies.
-#' @param formula An object of class "formula": a symbolic description of the variable
-#' being predicted using pairs of other variables. The details of its specification are given under ‘Details’.
-#' @param dat Dataframe with rows as observations and columns as variables. 
+#' @param dat dataframe with rows as observations and columns as variables. 
 #' Variables must all be observed or transformed categorical with finite range spaces.
-#' @return 
+#' @param var character string representing the variable in 
+#' dataframe \code{dat} to be predicted by pairs of other variables in the dataframe \code{dat}.
+#' @return Upper triangular matrix giving the expected conditional entropies of pairs of variables 
+#' given as rows and columns of the matrix. The diagonal gives \emph{EH(X|Z) = H(X,Z) - H(Z)} , that is
+#' when only one variable is used to predict \code{var}.
 #' @details The expected conditional entropy given by\cr
 #' 
-#' \emph{EH(Z|X,Y)= H(X,Y,Z) - H(X, Y) \cr
+#' \emph{EH(Z|X,Y) = H(X,Y,Z) - H(X, Y)} \cr
 #' 
-#' measures the prediction uncertainty when  pairs of variables \emph{X} and \emph{Y}
+#' measures the prediction uncertainty when pairs of variables \emph{X} and \emph{Y}
 #' are used to predict variable \emph{Z}.
-#' The lower the value of \emph{EH} given various pairs of variables \emph{X} and \emph{Y}, 
+#' The lower the value of \emph{EH} given different pairs of variables \emph{X} and \emph{Y}, 
 #' the stronger is the prediction of \emph{Z}.
 #' @author Termeh Shafie
 #' @seealso \code{\link{entropy_trivar}}, \code{\link{entropy_bivar}}
@@ -29,34 +31,26 @@
 
 #' @export
 
-prediction_power <- function(formula, dat) {
+prediction_power <- function(var, dat) {
+  z <- which(names(dat) == var)
   
-  # EH(Z|X,Y)=H(X,Y,Z)-H(X,Y) when Z and X and Y differ
-  
-  
-  # if three variables (two predictors)
-  # get trivariate entropies
+  varname.orig <- colnames(dat)
+  varname.new <- sprintf("V%d", 1:ncol(dat))
+  names(dat) <- varname.new
+
+  H2 <- entropy_bivar(dat)
   H3 <- entropy_trivar(dat)
+
     
   H3$V1 <- as.numeric(gsub("V", "", H3$V1))
   H3$V2 <- as.numeric(gsub("V", "", H3$V2))
   H3$V3 <- as.numeric(gsub("V", "", H3$V3))
   
-  # EH given as m matrices with the different Z variables
-  
-  # H(X,Z)+H(Y,Z)-H(Z)-H(X,Y,Z) 
   H3 <-  as.matrix(H3)
   dimE <- max(H3[,1:3])
-  loopZ <- sort(unique(c(H3[ ,1], H3[ ,2], H3[ ,3])))
-  EHZXY <- list()
-  EJXYZ <- list()
   
-  k=0
-  for(z in loopZ){
-    k=k+1
-    idz = which(apply(H3[,1:3], 1, function(x) any(x==z)))
-    EHZXY[[k]]=matrix(NA,dimE,dimE)
-    EJXYZ[[k]]=matrix(NA,dimE,dimE)
+  idz = which(apply(H3[,1:3], 1, function(x) any(x==z)))
+    EHZXY=matrix(NA,dimE,dimE)
     for(xy in idz){
       idxy=sort(setdiff(H3[xy,1:3],z))
       x=idxy[1]
@@ -64,34 +58,24 @@ prediction_power <- function(formula, dat) {
       x.coord=which(paste0("V",x)==colnames(H2))
       y.coord=which(paste0("V",y)==colnames(H2))
       z.coord=which(paste0("V",z)==colnames(H2))
-      EHZXY[[k]][x,y]=H3[xy,4]-H2[x.coord,y.coord]  
-      EJXYZ[[k]][x,y]=H2[x.coord,z.coord]+H2[y.coord,z.coord]-H2[z.coord,z.coord]-H3[xy,4]
-    }
-  }
+      EHZXY[x,y]=H3[xy,4]-H2[x.coord,y.coord]  
+      }
   
-  #add the diagonal giving EH(X|Z)=H(X,Z)-H(Z) 
-  k=0
-  for(z in loopZ){
-    z.coord=which(str_c("V",z)==colnames(H2))
-    k=k+1
+  #add the diagonal
+    z.coord=which(paste0("V",z)==colnames(H2))
+    H2[lower.tri(H2)] <- H2[upper.tri(H2)]
     for(x in 1:nrow(H2)){
       if(x!=z){
-        EHZXY[[k]][x,x]= H2[x,z.coord]-H2[z.coord,z.coord] 
+        EHZXY[x,x]= H2[x,z.coord]-H2[z.coord,z.coord] 
       }
     }
-  }
   
-  
-# create directory to save EH and EJ outputs
-# dir.create("EH")
-# dir.create("EJ")
-
-sapply(1:length(EHZXY),function(x) write.table(EHZXY[[x]],str_c("EHZYX_",loopZ[x],".csv"),
-                                               sep=",",col.names = F,row.names = F))
-sapply(1:length(EJXYZ),function(x) write.table(EHZXY[[x]],str_c("EJXYZ_",loopZ[x],".csv"),
-                                               sep=",",col.names = F,row.names = F))
-
-
-
-  
+    colnames(EHZXY) <- varname.orig
+    rownames(EHZXY) <- varname.orig
+    EHZXY <-  as.matrix(EHZXY)
+    EHZXY <- round(EHZXY, 3)
+    
+    return(EHZHY)
 }
+
+  
