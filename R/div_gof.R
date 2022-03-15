ae4c80c5435bf6ad0feff3cd8a525ceea9809a8c
termeh <termeh.shafie@manchester.ac.uk>
2022-03-15 14:08:31 +0000

    div_gof.R created

---
+++ R/div_gof.R
@@ -0,0 +1,3 @@
+#' @title Divergence Tests of Goodness of Fit
+#' @description Tests of various hypothetical structural models \emph{p0} against the general the model \emph{p} which is
+#' estimated using the empirical data.
