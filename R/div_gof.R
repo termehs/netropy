b7a3f25da860bb0cf27ca66db712cb3aa6919777
termeh <termeh.shafie@manchester.ac.uk>
2022-04-13 20:12:55 +0200

    div_gof.R updated

--- R/div_gof.R
+++ R/div_gof.R
@@ -40,7 +40,9 @@ div_gof <- function(dat, set1, set2, condition = 0) {
    # independent model, is this needed?
    q <- p["Sum",]*p[,"Sum"]
 
-   #chi.stat = 2nD(pq) where D = H_X + H_Y − H_(X,Y) = 2n J(X,Y)
+   # chi.stat = 2nD(pq) where D = H_X + H_Y − H_(X,Y) = 2n J(X,Y) pairwise
+   # conditional
+   # nested
   }
 
 
