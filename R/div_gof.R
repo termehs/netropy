8359c26337f59e629ed1a06f3487de6ed4f1d87e
termeh <termeh.shafie@manchester.ac.uk>
2022-04-28 13:43:05 +0200

    div_gof.R updated

--- R/div_gof.R
+++ R/div_gof.R
@@ -3,17 +3,18 @@
 #' the general model \emph{p} which is estimated using empirical data.
 #' @param dat dataframe with rows as observations and columns as variables.
 #' Variables must all be observed or transformed categorical with finite range spaces.
-#' @param var1 variable in \code{dat} specified to be independent to
+#' @param var1 character of variable name in \code{dat} specified to be independent to
 #' those specified in \code{var2} under \emph{p0}
-#' @param var2 variable in \code{dat} specified to be independent to
+#' @param var2 character of variable name in \code{dat} specified to be independent to
 #' those specified in \code{var1}
-#' @param var_cond variable in \code{dat} to condition the independence specification on,
+#' @param var_cond character of variable name in \code{dat} to condition the independence specification on,
 #' must be different variables than those specified in \code{var1} and \code{var2}).
 #' Default empty (no conditioning).
 #' @return Message indicating whether the hypothesis with the specified independence model
 #' can be rejected or not on approximately 5% level of significance.
-#' \item{summary}{Data frame including the value of the Divergence and its degrees of freedom}
-#' @details this function is currently implemented to only test the goodness of fit of models specified as
+#' \item{summary}{Data frame including the value of the value of
+#' the divergence \emph{D} and its degrees of freedom}
+#' @details this function is currently implemented to only test goodness of fit of models specified as
 #' \emph{X} independent of \emph{Y}, or \emph{X} independent of \emph{Y} given \emph{Z}.
 #' @author Termeh Shafie
 #' @seealso \code{\link{joint_entropy}},   \code{\link{assoc_graph}},  \code{\link{entropy_trivar}}
@@ -24,6 +25,8 @@
 #' Nowicki, K., Shafie, T., & Frank, O. (Forthcoming 2022). \emph{Statistical Entropy Analysis of Network Data}.
 #' @examples
 #'
+#' @export
+
 div_gof <- function(dat, var1, var2, var_cond = NULL) {
   idx_var1 <- which(names(dat) == var1)
   idx_var2 <- which(names(dat) == var2)
@@ -53,13 +56,12 @@ div_gof <- function(dat, var1, var2, var_cond = NULL) {
     # critical value at 5% level
       cv = df_chi2 + sqrt((8*df_chi2))
         if (chi2 > cv) {
-          return(message("the specified model of independence cannot be rejected"))
+          message("the specified model of independence cannot be rejected")
         }
         else if (chi2 <= cv) {
-          return(message("the specified model of independence is rejected"))
+          message("the specified model of independence is rejected")
         }
   }
-
 
   # conditional independence
   else if (length(idx_var1) == 1 & length(idx_var2) == 1 & length(idx_cond) == 1) {
@@ -81,19 +83,25 @@ div_gof <- function(dat, var1, var2, var_cond = NULL) {
     # critical value at 5% level
     cv = df_chi2 + sqrt((8*df_chi2))
     if (chi2 > cv) {
-      return(message("the specified model of independence cannot be rejected"))
+      message("the specified model of independence cannot be rejected")
     }
     else if (chi2 <= cv) {
-      return(message("the specified model of independence is rejected"))
+      message("the specified model of independence is rejected")
     }
-  }
-
+}
 
   # warning message for wrongful model specification
   else {
     warning("function currently implemented for tests of the kind X is indepndent of Y: Z _|_Y,
             and X is independent of Y given Z: X_|_Y|Z ")
   }
+
+    # output: test summary
+    #    p <- round(1 - pchisq(chi2, df_chi2),3)
+    summary <- as.data.frame(cbind(round(D,2), df_chi2))
+    rownames(summary)<- c()
+    colnames(summary) <- c('D', 'df(D)')
+    return(summary)
 }
 
 
